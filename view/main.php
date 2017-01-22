cbe9ca15d9fd0053cbefa7f8f6291d6f5d7694d4
ananina <e.ananina@inbox.ru>
2017-01-22 16:54:06 +0700

    Добавлена верстка главной страницы

--- view/main.php
+++ view/main.php
@@ -2,13 +2,22 @@
     <!-- /карусель логотипов, загрузка из json -->
     <div  class="multiple-items"></div>
 
-    <div class="left-sidebar">
-        <div class="tree-menu" id="tree-menu">
-            <ul class="category">
-            </ul>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="left-sidebar col-md-3">
+                <div class="tree-menu" id="tree-menu">
+                    <ul class="category">
+                    </ul>
+                </div>
+            </div>
+            <div class="content-right col-md-9">
+                <div class="main-content">
+                    <div id="img-info">
+                        <img src="<?=FPATH?>img/skigo15_rus_1.png">
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-    <div id="content">
 
-    </div>
 </div>
